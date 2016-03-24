c619c8cd585aa8e4578c940b6768e5229560dfa4
JDarkGreen <jgomez.4net@gmail.com>
2016-03-23 19:19:18 -0500

    update footer

--- themes/damol/footer.php
+++ themes/damol/footer.php
@@ -1,6 +1,6 @@
 
 <!-- Extraer opciones  -->
-<?php $options = get_option('damol_custom_settings'); var_dump($options); ?>
+<?php $options = get_option('damol_custom_settings'); ?>
 	
 	<!-- Footer -->
 	<footer class="mainFooter">
@@ -14,38 +14,38 @@
 					<!-- Ritulo -->
 					<h2 class="text-uppercase"><?php _e( 'damol sac.', 'damol-framework' ); ?></h2>
 
-					<!-- Contenedor flexible -->
-					<div class="item-prefooter__flexbox">
+					<!-- Contenedor -->
+					<div class="section-wrapper-relative">
 						<!-- Logo  -->
-						<figure><img src="" alt="" class="img-responsive" /></figure>
+						<figure class="logo__black"><img src="<?= IMAGES ?>/footer/damol_logo_black.jpg" alt="damol_logo_black" class="img-responsive" /></figure>
 						<!-- Parrafo -->
 						<?php $info_nosotros = $options['widget_nosotros']; if( !empty($info_nosotros) ) : ?>
-							<p><?= $info_nosotros ?></p>
+							<p class="info__about text-justify"><?= $info_nosotros ?></p>
 						<?php endif; ?>
 						<!-- Imagen -->
 						<?php $img_nosotros = $options['image_nosotros']; if( !empty($img_nosotros) ) : ?>
-							<figure><img src="<?= $img_nosotros ?>" alt="nosotros-damol" class="img-responsive" /></figure>
+							<figure class="info__about-img"><img src="<?= $img_nosotros ?>" alt="nosotros-damol" class="img-responsive" /></figure>
 						<?php endif; ?>
-					</div>
+					</div> <!-- /.item-flexbox -->
 				</article>
 				<!-- Item de sociales -->
-				<article class="item-prefooter item-prefooter--sociales">
+				<article class="item-prefooter item-prefooter--sociales text-center">
 					<!-- Titulo -->
-					<h2 class="text-uppercase"><?php _e( 'redes sociales', 'damol-framework' ); ?></h2>
+					<h2 class="text-uppercase"><?php _e( 'redes <br /> sociales', 'damol-framework' ); ?></h2>
 
 					<!-- Lista -->
 					<ul class="item-prefooter__social-links">
 						<!-- youtube -->
 						<?php $youtube = $options['red_social_ytube']; if( !empty($youtube) ) : ?>
-							<li><a href="<?= $youtube ?>"><img src="" alt="" class="img-responsive"></a></li>
+							<li><a href="<?= $youtube ?>"><img src="<?= IMAGES ?>/footer/redes-sociales/youtube.png" alt="facebook" class="img-responsive"></a></li>
 						<?php endif; ?>
 						<!-- twitter -->
 						<?php $twitter = $options['red_social_twitter']; if( !empty($twitter) ) : ?>
-							<li><a href="<?= $twitter ?>"><img src="" alt="" class="img-responsive"></a></li>
+							<li><a href="<?= $twitter ?>"><img src="<?= IMAGES ?>/footer/redes-sociales/twitter.png" alt="" class="img-responsive"></a></li>
 						<?php endif; ?>
 						<!-- fb -->
 						<?php $facebook = $options['red_social_fb']; if( !empty($facebook) ) : ?>
-							<li><a href="<?= $facebook ?>"><img src="" alt="" class="img-responsive"></a></li>
+							<li><a href="<?= $facebook ?>"><img src="<?= IMAGES ?>/footer/redes-sociales/facebook.png" alt="" class="img-responsive"></a></li>
 						<?php endif; ?>
 						
 					</ul>
@@ -66,8 +66,13 @@
 					<?php endif; ?>
 
 					<!-- Consultas Correo  -->
-					<?php $emails = $options['contact_emails']; if( !empty($emails) ) : ?>
-						<p><?php _e( 'Consultas:', 'damol-framework' ); ?></p>
+					<?php $emails = $options['contact_email']; if( !empty($emails) ) : ?>
+						<p><?php _e( 'Consultas:', 'damol-framework' ); 
+							$emails = explode(',', $emails);
+							foreach ($emails as $mail ) {
+								echo "<span class='text-orange'>" . $mail . "</span><br />";
+							}	
+						?></p>
 					<?php endif; ?>
 				</article> <!-- /. -->
 			</div> <!-- /.mainFooter__content -->			
@@ -77,6 +82,7 @@
 		<section class="mainFooter__info">
 			<!-- Contenedor limitador  -->
 			<div class="mainFooter__content">
+				<p></p>
 			</div> <!-- /.mainFooter__content -->
 		</section><!-- /.mainFooter__info -->
 		
